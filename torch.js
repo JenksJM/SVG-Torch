d33aefeed3dc32122eaeb28c15c26d32bdd0374f
jenksjm <jenksjm@gmail.com>
2020-06-27 19:25:09 +0100

    content commit

---
+++ torch.js
@@ -0,0 +1,57 @@
+class torchSVG {
+    constructor(url, documentID, location) {
+        this._imageURL = url;
+        this._elementID = documentID;
+        this._parentElementID = location;
+
+        this.outputImage();
+        this.addEventListeners();
+    }
+
+    // Methods
+
+    //image and holder output
+    outputImage() {
+        var imageHtml = "<div class='svgTourch' id='" + this._elementID + "'>";
+        imageHtml += "<img src='" + this._imageURL + "'>";
+        imageHtml += "</div>";
+        document.getElementById(this._parentElementID).innerHTML = imageHtml;
+    }
+
+    //image and holder output
+    addEventListeners() {
+        document.getElementById(this._elementID).addEventListener('mousemove', this.updateTorchPosition);
+        document.getElementById(this._elementID).addEventListener('touchmove', this.updateTorchPosition);
+    }
+
+    //update Torch Position
+    updateTorchPosition = e => {
+        var x = e.clientX || e.touches[0].clientX;
+        var y = e.clientY || e.touches[0].clientY;
+
+        document.getElementById(this._elementID).style.setProperty('--cursorX', x + 'px')
+        document.getElementById(this._elementID).style.setProperty('--cursorY', y + 'px')
+    }
+
+    //Image URL getter/setter
+    get imageURL() {
+        return this._imageURL;image
+    }
+    set imageURL(x) {
+        this._imageURL = x;
+    }
+
+    //elementID getter/setter
+    get elementID() {
+        return this._elementID;
+    }
+    set elementID(x) {
+        this._elementID = x;
+    }
+
+    //parentElementID getter/setter
+    get parentElementID() {
+        document.getElementById("passage").innerHTML = "Paragraph changed!";
+        this._parentElementID = x;
+    }
+}
